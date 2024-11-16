aa4962565b5ac7a6dc38b0a6943fcf0c660181b9
shikharUserTest2 <shikhar.rm+test2@metapercept.com>
2024-11-16 17:45:38 +0530

    fix: hosting issues and cleanup

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import "./Login.css";
-import Register from "./Register";
-import SignIn from "./SignIn";
+import React, { useState } from 'react'
+import './Login.css'
+import Register from './Register'
+import SignIn from './SignIn'
+import LinkedInLogo from './images/linkedin.png'
 
-function Login() {
-
-  const [signinShow, setSigninShow] = useState(true);
-  const [registerShow, setRegisterShow] = useState(false);
+function Login () {
+  const [signinShow, setSigninShow] = useState(true)
+  const [registerShow, setRegisterShow] = useState(false)
 
   const handleSignInClick = () => {
     setRegisterShow(false)
@@ -19,24 +19,27 @@ function Login() {
   }
 
   return (
-    <div className="login">
-
-      <img
-        src="https://th.bing.com/th/id/R.1e905ce98876c9742e892bbb029f5a4f?rik=3LMZyFzgHKPMIQ&riu=http%3a%2f%2fblog.doteasy.com%2fwp-content%2fuploads%2f2013%2f12%2flinkedin-logo.png&ehk=uMGZ6PcFMad7tG1wxs6hRZN7RUwbi9h2RoI0x2rYUDQ%3d&risl=&pid=ImgRaw&r=0"
-        alt="LinkedIn"
-      />
-
-      <div className="login__options">
-        <div onClick={handleSignInClick} style={{background: signinShow ? "#0074b1" : "initial"}}>SignIn</div>
-        <div onClick={handleRegisterClick} style={{background: registerShow ? "#0074b1" : "initial"}}>Register</div>
+    <div className='login'>
+      <img src={LinkedInLogo} alt='LinkedIn' />
+      <div className='login__options'>
+        <div
+          onClick={handleSignInClick}
+          style={{ background: signinShow ? '#0074b1' : 'initial' }}
+        >
+          SignIn
+        </div>
+        <div
+          onClick={handleRegisterClick}
+          style={{ background: registerShow ? '#0074b1' : 'initial' }}
+        >
+          Register
+        </div>
       </div>
 
-      
       <SignIn show={signinShow} />
       <Register show={registerShow} />
-
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
